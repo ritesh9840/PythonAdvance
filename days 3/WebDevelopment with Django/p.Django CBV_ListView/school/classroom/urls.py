c036c6f300c561cc78ec415e836cee2e78522878
ritesh Singh <52201727+ritesh9840@users.noreply.github.com>
2024-02-11 13:13:45 +0545

    upto CBV_Delete

---
+++ days 3/WebDevelopment with Django/p.Django CBV_ListView/school/classroom/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import HomeView, ThankYouView,ContactFormView,TeacherCreateView,TeacherListView
+
+app_name = 'classroom'
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('thank_you/', ThankYouView.as_view(), name='thank_you'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('create_teacher/', TeacherCreateView.as_view(), name='create_teacher'),
+    path('teacher_list/', TeacherListView.as_view(), name='teacher_list'),
+]
---
+++ days 3/WebDevelopment with Django/p.Django CBV_ListView/school/classroom/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, FormView,CreateView,ListView
+from classroom.forms import ContactForm
+from classroom.models import Teacher
+from django.urls import reverse_lazy
+
+
+class HomeView(TemplateView):
+    template_name = 'classroom/home.html'
+
+
+class ThankYouView(TemplateView):
+    template_name = 'classroom/thank_you.html'
+    
+class TeacherCreateView(CreateView):
+    model=Teacher
+    #automaticaly look for teache_form.html
+    fields="__all__"
+    success_url=reverse_lazy('classroom:thank_you')
+
+
+class TeacherListView(ListView):
+    model = Teacher
+    queryset=Teacher.objects.order_by('first_name')
+    context_object_name="teacher_list"
+
+class ContactFormView(FormView):
+    form_class = ContactForm
+    template_name = 'classroom/contact.html'
+    success_url = "/classroom/thank_you"
+
+    def form_valid(self, form):
+        # Process the form data here
+        print(form.cleaned_data)
+        # You can save the form data to the database or perform any other actions here
+        return super().form_valid(form)
